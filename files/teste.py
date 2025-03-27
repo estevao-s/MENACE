17e528cb77d37e0afffa8f7e158ce085ab164a90
Daniel R. Cassar <daniel.r.cassar@gmail.com>
2025-03-27 14:10:35 -0300

    atualização do teste.py

--- files/teste.py
+++ files/teste.py
@@ -1,36 +1,26 @@
 from api import Jogador, simulacao, plot
 from pprint import pprint
-import numpy as np
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
     num_jogos = 1000
 
     player1 = Jogador(1)
-    # player2 = Jogador(2, reforco_vitoria=0, reforco_derrota=0)
+    player2 = Jogador(2, reforco_vitoria=0, reforco_derrota=0)
 
-    player3 = Jogador(1, reforco_vitoria=0, reforco_derrota=0)
-    player4 = Jogador(2)
+    # player3 = Jogador(1, reforco_vitoria=0, reforco_derrota=0)
+    # player4 = Jogador(2)
 
-    # player1, player2, vitorias1, vitorias2, empates12 = simulacao(
-    #     player1, player2, num_jogos
-    # )
-
-    # player3, player4, vitorias3, vitorias4, empates34 = simulacao(
-    #     player3, player4, num_jogos
-    # )
-
-    num_jogos = 10000
-
-    player1, player4, vitorias1b, vitorias4b, empates14 = simulacao(
-        player1, player4, num_jogos
+    playerA, playerB, vitoriasA, vitoriasB, empates = simulacao(
+        player1,
+        player2,
+        num_jogos,
     )
 
     print()
-    pprint(player1.brain)
+    pprint(playerA.brain)
     print("---")
-    pprint(player4.brain)
+    pprint(playerB.brain)
 
-    plot_name = "simulacao3"
-    plot(vitorias1b, vitorias4b, empates14, plot_name, show=True)
-
+    plot_name = "simulacao"
+    plot(vitoriasA, vitoriasB, empates, plot_name, show=True)
